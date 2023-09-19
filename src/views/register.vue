592c65f61529758a0272589700bdd850c058d356
Wyatt VO <votranquy96@gmail.com>
2023-09-19 23:13:12 +0700

    Register Page Layout

---
+++ src/views/register.vue
@@ -0,0 +1,73 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="mt-8">
+    <div class="container mx-auto px-8">
+      <!-- Start Form -->
+      <form
+        class="flex flex-col justify-start space-y-6"
+        @submit.prevent="onSubmit"
+      >
+        <div class="row">
+          <label class="flex flex-col" for="fullName">
+            <span class="font-semibold">Full Name</span>
+            <input
+              id="fullName"
+              class="px-4 py-3 rounded-lg border border-gray-100 mt-1"
+              type="text"
+              placeholder="iMoney..."
+            />
+          </label>
+        </div>
+        <div class="row">
+          <label class="flex flex-col" for="email">
+            <span class="font-semibold">Email</span>
+            <input
+              id="email"
+              class="px-4 py-3 rounded-lg border border-gray-100 mt-1"
+              type="email"
+              placeholder="abc@gmail.com"
+            />
+          </label>
+        </div>
+        <div class="row">
+          <label class="flex flex-col" for="password">
+            <span class="font-semibold">Password</span>
+            <input
+              id="password"
+              class="px-4 py-3 rounded-lg border border-gray-100 mt-1"
+              type="password"
+              placeholder=""
+            />
+          </label>
+        </div>
+        <div class="row">
+          <button
+            class="py-3 text-center w-full bg-primary text-white font-bold rouded"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+      <!-- Start Direction -->
+      <div class="w-full text-center mt-6">
+        <span class="font-semibold">I'm already a member</span>
+        <span class="ml-1">
+          <router-link
+            :to="{ name: 'login', param: {} }"
+            class="text-primary font-bold"
+            >Sign In
+          </router-link>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  setup() {
+    function onSubmit() {}
+    return { onSubmit };
+  },
+};
+</script>
