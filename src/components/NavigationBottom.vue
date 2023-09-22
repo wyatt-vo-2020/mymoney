2bff2ab1a04df553d0979c13fd6c82aa868ddc44
Wyatt VO <votranquy96@gmail.com>
2023-09-22 23:36:36 +0700

    UI for navigation bottom

---
+++ src/components/NavigationBottom.vue
@@ -0,0 +1,69 @@
+<template>
+  <footer
+    id="footer"
+    class="fixed w-full bottom-0 left-0 bg-white pt-4 p-b-safe-4"
+  >
+    <div class="container mx-auto px-8 h-full">
+      <ul class="flex justify-between h-full items-center">
+        <li v-for="navItem in navItems" :key="navItem.name">
+          <router-link to="/">
+            <i class="t2ico text-2xl" :class="navItem.icon"></i>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const navItems = reactive([
+      {
+        icon: "t2ico-category",
+        text: "Home",
+        name: "home",
+      },
+      {
+        icon: "t2ico-chart",
+        text: "Chart",
+        name: "home",
+      },
+      {
+        icon: "t2ico-presentation",
+        text: "Budget",
+        name: "home",
+      },
+      {
+        icon: "t2ico-profile",
+        text: "Profile",
+        name: "profile",
+      },
+    ]);
+    return { navItems };
+  },
+};
+</script>
+<style lang="css" scoped>
+footer#footer::before {
+  @apply absolute;
+  @apply bg-dark-light;
+
+  content: "";
+  width: 60px;
+  height: 30px;
+  border-bottom-left-radius: 60px;
+  border-bottom-right-radius: 60px;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+footer#footer::after {
+  @apply absolute -top-6 flex items-center justify-center text-white;
+  @apply w-12 h-12 bg-primary rounded-full cursor-pointer;
+  content: "\e921";
+  font-family: "t2ico";
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Default Layout</h1>
   <slot />
+  <navigation-bottom />
 </template>
+<script>
+import NavigationBottom from "@/components/NavigationBottom.vue";
+export default {
+  components: { NavigationBottom },
+};
+</script>
