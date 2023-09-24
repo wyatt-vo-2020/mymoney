ad59441d20355d38feb404ad878e4afea9c0c93c
Wyatt VO <votranquy96@gmail.com>
2023-09-24 11:54:08 +0700

    Refactoring plus button code

--- src/components/NavigationBottom.vue
+++ src/components/NavigationBottom.vue
@@ -3,7 +3,7 @@
     id="footer"
     class="fixed w-full bottom-0 left-0 bg-white pt-4 p-b-safe-4"
   >
-    <div class="container mx-auto px-8 h-full">
+    <div class="container mx-auto px-8 h-full left-1/2">
       <ul class="flex justify-between h-full items-center">
         <li v-for="navItem in navItems" :key="navItem.name">
           <router-link :to="{ name: navItem.name, param: {} }">
@@ -11,6 +11,16 @@
           </router-link>
         </li>
       </ul>
+    </div>
+    <div
+      data-circle
+      class="absolute top-0 left-1/2 transform -translate-x-1/2"
+    ></div>
+    <div
+      data-circle-inset
+      class="absolute flex items-center justify-center bg-primary text-white w-12 h-12 rounded-full cursor-pointer -top-6 left-1/2 transform -translate-x-1/2"
+    >
+      <i class="t2ico t2ico-plus"></i>
     </div>
   </footer>
 </template>
@@ -25,30 +35,19 @@ export default {
 };
 </script>
 <style lang="css" scoped>
-footer#footer::before {
-  @apply absolute;
+footer#footer > div[data-circle] {
   @apply bg-dark-light;
 
-  content: "";
   width: 60px;
   height: 30px;
   border-bottom-left-radius: 60px;
   border-bottom-right-radius: 60px;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
 }
-footer#footer::after {
-  @apply absolute -top-6 flex items-center justify-center text-white;
-  @apply w-12 h-12 bg-primary rounded-full cursor-pointer;
-  content: "\e921";
-  font-family: "t2ico";
-  left: 50%;
-  transform: translateX(-50%);
-}
+
 footer#footer ul li :nth-child(2) {
   margin-right: 10%;
 }
+
 footer#footer ul li :nth-child(3) {
   margin-left: 10%;
 }
